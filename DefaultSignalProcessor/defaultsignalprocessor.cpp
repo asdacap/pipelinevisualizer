970b7e579381019c16784ce7d770ab44f082e34d
Muhd Amirul Ashraf <asdacap@gmail.com>
2011-03-20 21:21:00 +0800

    Fixed double multiply naming.

--- DefaultSignalProcessor/defaultsignalprocessor.cpp
+++ DefaultSignalProcessor/defaultsignalprocessor.cpp
@@ -61,7 +61,7 @@ DefaultSignalProcessor::DefaultSignalProcessor()
 
 QList<PipeProcessGraphicsProvider*> DefaultSignalProcessor::getProviders(PVisual *pv){
     QList<PipeProcessGraphicsProvider*> thelist;
-    thelist.append(new SimpleProcessGraphicsProviderTemplate<DoubleMultiply>("DoubleMultiplyExternal",pv,0,0,2,1,0,0));
+    thelist.append(new SimpleProcessGraphicsProviderTemplate<DoubleMultiply>("DoubleMultiply",pv,0,0,2,1,0,0));
     thelist.append(new SoundSinkProvider(pv));
     thelist.append(new SoundFeederProvider(pv));
     thelist.append(new GraphAbstractionProvider(pv));
